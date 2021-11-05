8df6625297791f33f1ed933ff3302d58c4c3b4b3
Pavel Dubrova <pashadubrova@gmail.com>
2021-11-05 13:57:32 +0200

    clk: qcom: Add the clock controller drivers for SDM845
    Add drivers for global, camera, debug, display, gpu and video
    clock controller support on SDM845.
    This snapshot of the camcc-sdm845, debugcc-sdm845, dispcc-sdm845,
    gcc-sdm845, gpucc-sdm845 and videocc-sdm845 drivers as of
    https://source.codeaurora.org/quic/la/kernel/msm-4.9
    commit 7e5c05f ("Merge "msm: camera: jpeg: Ensure in/out map entries are
    within allowed range"").
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP PREEMPT Sat Nov 16 14:38:29 CST 2024"
+#define LINUX_COMPILE_BY "gl"
+#define LINUX_COMPILE_HOST "gl-linux"
+#define LINUX_COMPILER "Android (7284624, based on r416183b) clang version 12.0.5 (https://android.googlesource.com/toolchain/llvm-project c935d99d7cf2016289302412d708641d52d2f7ee), GNU ld (GNU Binutils for Ubuntu) 2.42"
